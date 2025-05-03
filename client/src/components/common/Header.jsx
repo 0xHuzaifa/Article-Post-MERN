a6f6b1680f31e0703e154e616d72e24232dace94
Huzaifa Ahmed <huzaifasaleem098@gmail.com>
2025-05-03 12:00:31 +0500

    changes in navbar

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -51,62 +51,73 @@ export default function Header() {
   );
 
   return (
-    <div className="max-w-xs sm:max-w-lg md:max-w-2xl lg:max-w-5xl mx-auto my-4 lg:px-2">
-      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-3xl px-4 py-2 lg:px-8 lg:py-4">
-        <div className="flex items-center justify-between text-blue-gray-900">
-          <Typography
-            as="a"
-            href="#"
-            className="mr-4 cursor-pointer py-1.5 font-medium"
-          >
-            Article Post
-          </Typography>
-          <div className="flex items-center gap-4">
-            <div className="mr-4 hidden lg:block">{navList}</div>
-            <div className="flex items-center gap-x-1">
-              <Button
-                variant="text"
-                size="sm"
-                className="hidden lg:inline-block"
-                onClick={() => navigate("/login")}
-              >
-                <span>Log In</span>
-              </Button>
-              <Button
-                variant="gradient"
-                size="sm"
-                className="hidden lg:inline-block"
-                onClick={() => navigate("/signup")}
-              >
-                <span>Sign Up</span>
-              </Button>
-            </div>
-            <IconButton
+    <Navbar className="sticky top-0 z-10 h-max max-w-full shadow-none">
+      <div className="max-w-5xl mx-auto flex items-center justify-between rounded-[30px] px-4 py-2 lg:px-8 lg:py-4 shadow-lg">
+        <Typography
+          className="mr-4 cursor-pointer py-1.5 font-medium text-blue-gray-900"
+          variant="h5"
+          onClick={() => navigate("/")}
+        >
+          Article Post
+        </Typography>
+        <div className="flex items-center gap-4">
+          {/* navbar list */}
+          <div className="mr-4 hidden lg:block">{navList}</div>
+          <div className="flex items-center gap-x-1">
+            {/* login button */}
+            <Button
               variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-              ripple={false}
-              onClick={() => setOpenNav(!openNav)}
+              size="sm"
+              className="hidden lg:inline-block"
+              onClick={() => navigate("/login")}
             >
-              {openNav ? (
-                <XMarkIcon className="h-6 w-6" />
-              ) : (
-                <Bars3Icon className="h-6 w-6" />
-              )}
-            </IconButton>
-          </div>
-        </div>
-        <MobileNav open={openNav}>
-          {navList}
-          <div className="flex items-center gap-x-1 flex-col">
-            <Button fullWidth variant="text" size="sm" className="mb-2">
               <span>Log In</span>
             </Button>
-            <Button fullWidth variant="gradient" size="sm">
-              <span>Sign Up</span>
+            {/* signup button */}
+            <Button
+              variant="gradient"
+              size="sm"
+              className="hidden lg:inline-block"
+              onClick={() => navigate("/signup")}
+            >
+              <span>Sign in</span>
             </Button>
           </div>
-        </MobileNav>
-      </Navbar>
-    </div>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <XMarkIcon className="h-6 w-6" />
+            ) : (
+              <Bars3Icon className="h-6 w-6" />
+            )}
+          </IconButton>
+        </div>
+      </div>
+      <MobileNav open={openNav}>
+        {navList}
+        <div className="flex items-center gap-x-1">
+          <Button
+            fullWidth
+            variant="text"
+            size="sm"
+            onClick={() => navigate("/login")}
+          >
+            <span>Log In</span>
+          </Button>
+          <Button
+            fullWidth
+            variant="gradient"
+            size="sm"
+            onClick={() => navigate("/signup")}
+          >
+            <span>Sign in</span>
+          </Button>
+        </div>
+      </MobileNav>
+    </Navbar>
   );
 }
