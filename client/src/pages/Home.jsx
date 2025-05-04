360e6719c7c7e418c8919083f99eca3c3dc77283
Huzaifa Ahmed <huzaifasaleem098@gmail.com>
2025-05-04 23:55:12 +0500

    add: home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import HeroSection from "@/components/HeroSection";
+import { ArticleCard } from "@/components/common/ArticleCard";
+import { Typography } from "@material-tailwind/react";
+
+const AllArticles = [
+  {
+    title: "UI/UX Review Check1",
+    description:
+      "The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to 'Naviglio' where you can enjoy the main night life in Barcelona.",
+    category: "Category",
+  },
+  {
+    title: "UI/UX Review Check2",
+    description:
+      "The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to 'Naviglio' where you can enjoy the main night life in Barcelona.",
+    category: "Category",
+  },
+  {
+    title: "UI/UX Review Check3",
+    description:
+      "The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to 'Naviglio' where you can enjoy the main night life in Barcelona.",
+    category: "Category",
+  },
+  {
+    title: "UI/UX Review Check4",
+    description:
+      "The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to 'Naviglio' where you can enjoy the main night life in Barcelona.",
+    category: "Category",
+  },
+  {
+    title: "UI/UX Review Check5",
+    description:
+      "The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to 'Naviglio' where you can enjoy the main night life in Barcelona.",
+    category: "Category",
+  },
+  {
+    title: "UI/UX Review Check6",
+    description:
+      "The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to 'Naviglio' where you can enjoy the main night life in Barcelona.",
+    category: "Category",
+  },
+];
 
 export default function Home() {
-  return <div>Home Page</div>;
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const someArticles = AllArticles.slice(0, 3);
+    setArticles(someArticles);
+  }, []);
+
+  return (
+    <div className="h-[1000px]">
+      <HeroSection />
+      <div className="w-full bg-blue-gray-50 px-5">
+        <Typography className="pt-14 text-xl md:text-4xl font-bold">
+          Latest Articles
+        </Typography>
+        <hr className="max-w-full mt-2 bg-blue-gray-900 rounded-lg" />
+        {articles && articles.length > 0 && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 mx-auto justify-items-center">
+            {articles.map((article) => (
+              <ArticleCard
+                key={article.title}
+                title={article.title}
+                category={article.category}
+                content={article.description}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
